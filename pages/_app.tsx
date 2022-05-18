6b3561c5f5782b8cf29fc531e0384df3d2c64fdc
Tanishq Sharma <tanishqsh@gmail.com>
2022-05-18 12:28:32 -0400

    Rainbowkit, Wagmi installed, landing page updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,43 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {
+  apiProvider,
+  configureChains,
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit'
+import '@rainbow-me/rainbowkit/styles.css'
+
+import { chain, createClient, WagmiProvider } from 'wagmi'
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [apiProvider.alchemy(process.env.ALCHEMY_ID), apiProvider.fallback()]
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'Rainbowkit Starter App',
+  chains,
+})
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+})
+
+/**
+ * Wrapping the app with WAGMI and Rainbowkit Provider
+ */
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiProvider client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiProvider>
+  )
 }
 
 export default MyApp
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'rainbow-gradient':
+          'linear-gradient(72.13deg, #1D3985 -1.09%, #8D55AC 18.51%, #C75149 38.11%, #F6C24B 57.11%, #5FCE6E 73.74%, #50B1D6 93.34%);',
+      },
+    },
   },
   plugins: [],
 }
